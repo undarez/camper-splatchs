a8a686a1af8cf38140563748114d39d720418555
votre-nom <votre-email@github.com>
2024-12-23 22:38:34 +0100

    modif code agent android apk 1.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': resolve(__dirname, './'),
-    }
-    return config
+const config = {
+  images: {
+    unoptimized: true,
   },
-};
-export default nextConfig;
+}
+
+export default config;
