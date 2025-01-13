fa262318ad967848d64569eeb9ce1499dc584bc2
votre-nom <votre-email@github.com>
2025-01-13 20:40:25 +0100

    session invité

--- app/pages/MapView/page.tsx
+++ app/pages/MapView/page.tsx
@@ -1,5 +1,33 @@
-import MapView from "./MapView";
+"use client";
+
+import { useState, useEffect } from "react";
+import { MapViewComponent } from "./MapViewComponent";
+import LoadingScreen from "@/app/components/Loader/LoadingScreen/page";
+import { type Station } from "@prisma/client";
 
 export default function MapViewPage() {
-  return <MapView />;
+  const [stations, setStations] = useState<Station[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchStations = async () => {
+      try {
+        const response = await fetch("/api/stations");
+        const data = await response.json();
+        setStations(data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des stations:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStations();
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
+  return <MapViewComponent stations={stations} />;
 }
